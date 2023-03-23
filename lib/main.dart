a2a89335c5e5d00115b1706ee656f506f44b1a66
monicaelisabet <monicaelisabet07@gmail.com>
2023-03-24 00:54:15 +0700

    prak_tpm1

---
+++ lib/main.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+class LoginPage extends StatelessWidget {
+  const LoginPage ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar, (title: 'Text("LoginPage )'text)
+        ,),
+      
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        backgroundColor: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: TextField(
+                controller: _usernameController,
+                decoration: InputDecoration(
+                  hintText: 'Username',
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: TextField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton (
+              child: Text('Login'),
+              onPressed: () {
+                // Perform login logic here
+                String username = _usernameController.text;
+                String password = _passwordController.text;
+                if (username == 'login' && password == '123') {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => HomePage()),
+                  );
+                } else {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Text('Error'),
+                        content: Text('Invalid username or password'),
+                        actions: <Widget>[
+                          TextButton (
+                            child: Text('OK'),
+                            onPressed: () => Navigator.pop(context),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Biodata Kelompok',
+              style: TextStyle(fontSize: 30),
+            ),
+            SizedBox(height: 5),
+            Text(
+              'Vanka Angelica Putri (123200061)',
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              'Monica Elisabet (123200082)',
+              style: TextStyle(fontSize: 20),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
